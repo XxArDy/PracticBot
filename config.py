8ce7ca2fb0cf411a5cf87d14b7b7bb9c906fd4ad
XxArDy <64437785+XxArDy@users.noreply.github.com>
2023-02-15 19:54:05 +0200

    ЗРОБИВ ВСЕ В СОЛО!!!!!

--- config.py
+++ config.py
@@ -3,4 +3,4 @@ from dataclasses import dataclass
 
 @dataclass
 class Config:
-    token: str = '5547578793:AAElHqW2iq56HyNzOtnNO5J1wNKvxyK7P0Y'
+    token: str = '5843840942:AAG0fbL3wO57oTIc9hHvTX90hkzW7A-XJWM'
